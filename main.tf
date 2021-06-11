68a5e3a00a26a5dd7f1b287f0dcbbceb3810ddd9
MessKon <35975404+MessKon@users.noreply.github.com>
2021-06-11 10:33:01 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "infrastructure-pipelines-workshop"
 
     workspaces {
-      name = "john-d-consul"
+      name = "kostas-m-consul"
     }
   }
   required_providers {
